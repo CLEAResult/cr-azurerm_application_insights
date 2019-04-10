303ae0d2a0510eb2195333a2451fccfb3c9cd0b9
David McPike <david.mcpike@clearesult.com>
2019-04-09 19:27:13 -0500

    add dynamic count to resource name; add naming module version reference

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 resource "azurerm_application_insights" "appinsights" {
   count               = "${var.count != "" ? var.count : "1"}"
-  name                = "${local.name}"
+  name                = "${local.name}${format("%3d", count.index)}"
   location            = "eastus"
   resource_group_name = "${var.rg_name}"
   application_type    = "Web"
+
   lifecycle {
     ignore_changes = [
-      "tags"
+      "tags",
     ]
   }
+
   tags {
     InfrastructureAsCode = "True"
   }
--- variables.tf
+++ variables.tf
@@ -3,21 +3,21 @@ variable "rgid" {
 }
 
 variable "location" {
-  default = "southcentralus"
+  default     = "southcentralus"
   description = "Location for resources to be created"
 }
 
 variable "count" {
-    default = "1"
+  default = "1"
 }
 
 variable "name_prefix" {
-  default = ""
+  default     = ""
   description = "Allows users to override the standard naming prefix.  If left as an empty string, the standard naming conventions will apply."
 }
 
 variable "environment" {
-  default = "dev"
+  default     = "dev"
   description = "Environment used in naming lookups"
 }
 
@@ -27,17 +27,17 @@ variable "rg_name" {
 
 # Compute default name values
 locals {
-  env_id              = "${lookup(module.naming.env-map, var.environment, "ENV")}"
-  type                = "${lookup(module.naming.type-map, "azurerm_application_insights", "TYP")}"
+  env_id = "${lookup(module.naming.env-map, var.environment, "ENV")}"
+  type   = "${lookup(module.naming.type-map, "azurerm_application_insights", "TYP")}"
 
   default_rgid        = "${var.rgid != "" ? var.rgid : "NORGID"}"
   default_name_prefix = "c${local.default_rgid}${local.env_id}"
 
-  name_prefix         = "${var.name_prefix != "" ? var.name_prefix : local.default_name_prefix}"
-  name                = "${local.name_prefix}${local.type}001"
+  name_prefix = "${var.name_prefix != "" ? var.name_prefix : local.default_name_prefix}"
+  name        = "${local.name_prefix}${local.type}"
 }
 
 # This module provides a data map output to lookup naming standard references
 module "naming" {
-  source = "git::ssh://git@github.com/clearesult/cr-azurerm-naming.git"
+  source = "git::ssh://git@github.com/clearesult/cr-azurerm-naming.git?ref=v1.0"
 }
