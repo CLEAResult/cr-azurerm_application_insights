12ea1fec6b74f7dc7130dec17e8c7b181cc3a23f
David McPike <david.mcpike@clearesult.com>
2019-10-04 19:35:32 -0500

    Add resource ID to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "instrumentation_key" {
   value = azurerm_application_insights.appinsights.*.instrumentation_key
 }
 
+output "id" {
+  value = azurerm_application_insights.appinsights.*.id
+}
+
 output "app_id" {
   value = azurerm_application_insights.appinsights.*.app_id
 }
