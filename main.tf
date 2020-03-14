ff6bae49d7760eba5b7c015038ffc8d4b05179f8
David McPike <7037366+mcdafydd@users.noreply.github.com>
2020-03-14 06:54:32 -0500

    Minor updates
        - Update naming module version to remove deprecation warnings
        - Expose all optional settings
        - Add types to variables

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 resource "azurerm_application_insights" "appinsights" {
-  name                = format("%s%03d", local.name, count.index + 1)
-  count               = var.num
-  location            = "eastus"
-  resource_group_name = var.rg_name
-  application_type    = "Web"
+  name                                  = format("%s%03d", local.name, count.index + 1)
+  count                                 = var.num
+  location                              = "eastus"
+  resource_group_name                   = var.rg_name
+  application_type                      = var.application_type
+  daily_data_cap_in_gb                  = var.daily_data_cap_in_gb
+  daily_data_cap_notifications_disabled = var.daily_data_cap_notifications_disabled
+  retention_in_days                     = var.retention_in_days
+  sampling_percentage                   = var.sampling_percentage
 
   lifecycle {
     ignore_changes = [tags]
--- variables.tf
+++ variables.tf
@@ -1,14 +1,18 @@
 variable "rgid" {
+  type        = string
   description = "RGID used for naming."
 }
 
 variable "location" {
+  type        = string
   default     = "southcentralus"
   description = "Location for resources to be created"
 }
 
 variable "num" {
-  default = "1"
+  type        = number
+  default     = 1
+  description = "Number of resources to be created"
 }
 
 variable "name_prefix" {
@@ -17,16 +21,49 @@ variable "name_prefix" {
 }
 
 variable "environment" {
+  type        = string
   default     = "dev"
   description = "Environment used in naming lookups"
 }
 
 variable "rg_name" {
+  type        = string
   description = "Resource group name"
 }
 
 variable "subscription_id" {
+  type        = string
   description = "Prompt for subscription ID"
+}
+
+variable "application_type" {
+  type        = string
+  default     = "other"
+  description = "Specifies the type of Application Insights to create. Valid values are ios for iOS, java for Java web, MobileCenter for App Center, Node.JS for Node.js, other for General, phone for Windows Phone, store for Windows Store and web for ASP.NET. Please note these values are case sensitive; unmatched values are treated as ASP.NET by Azure. Changing this forces a new resource to be created. Default is other."
+}
+
+variable "daily_data_cap_in_gb" {
+  type        = number
+  default     = 100
+  description = "Specifies the Application Insights component daily data volume cap in GB. Default is 100GB. Per Microsoft: \"Use care when you set the daily cap. Your intent should be to never hit the daily cap. If you hit the daily cap, you lose data for the remainder of the day, and you can't monitor your application.\""
+}
+
+variable "daily_data_cap_notifications_disabled" {
+  type        = bool
+  default     = false
+  description = "Specifies if a notification email will be send when the daily data volume cap is met. Default is false. Notifications will be sent to the default roles set by Microsoft."
+}
+
+variable "retention_in_days" {
+  type        = number
+  default     = 90
+  description = "Specifies the retention period in days. Possible values are 30, 60, 90, 120, 180, 270, 365, 550 or 730. Default is 90."
+}
+
+variable "sampling_percentage" {
+  type        = number
+  default     = 100
+  description = "Specifies the percentage of the data produced by the monitored application that is sampled for Application Insights telemetry. Default is 100 percent."
 }
 
 # Compute default name values
@@ -49,6 +86,6 @@ locals {
 
 # This module provides a data map output to lookup naming standard references
 module "naming" {
-  source = "git::https://github.com/CLEAResult/cr-azurerm-naming.git?ref=v1.1.0"
+  source = "git::https://github.com/CLEAResult/cr-azurerm-naming.git?ref=v1.1.2"
 }
 
